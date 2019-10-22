a059fb9c82eff403462498ba75e7016ec3508c05
nathanmailhiot <nathanjm12@outlook.com>
2019-10-22 12:52:43 -0400

    redux form and survey new

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        SurveyNew!
+        <h3>wow</h3>
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
