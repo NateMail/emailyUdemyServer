f2fc735f206389c932774e2a16ae93b3f5f9f1d3
nathanmailhiot <nathanjm12@outlook.com>
2019-10-22 14:54:29 -0400

    user able to create a form and review

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import formFields from './formFields';
 
-const SurveyReview = () => {
+const SurveyReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
-      <h5>Please confirm </h5>
-      <h6>your entries</h6>
+      <h5>Please confirm your entries </h5>
+      {reviewFields}
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+        Back
+      </button>
     </div>
   );
 };
 
-export default SurveyReview;
+function mapStateToProps(state) {
+  return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyReview);
