258aa42f015817dd3bb4043bc7a4487bb9c43cb8
nathanmailhiot <nathanjm12@outlook.com>
2019-10-22 14:32:36 -0400

    Form review redirect after validation

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SurveyReview = () => {
+  return (
+    <div>
+      <h5>Please confirm </h5>
+      <h6>your entries</h6>
+    </div>
+  );
+};
+
+export default SurveyReview;
